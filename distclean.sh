0a458642c443cd37d2bbdc6d4f757642408cd8b0
thomas <mrvelle@gmail.com>
2023-10-22 13:39:04 +0200

    typo in distclean.sh

--- distclean.sh
+++ distclean.sh
@@ -5,7 +5,7 @@
 # This script is meant to be called by _make target_ 'distclean',
 # and this code could actually just as well be placed there. Should it?
 # I have placed it here for now, because I am cautious with touching the
-# Makefile(.in) too much. 
+# Makefile(.in) too much.
 
 # Naming `distclean` given in accordance with Make target naming heuristics:
 # https://www.gnu.org/software/automake/manual/html_node/Standard-Targets.html.
